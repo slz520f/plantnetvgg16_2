3e4340a15fabdc165acca809ab2c0686ed6f95be
slz520f <zgs471455251@gmail.com>
2024-09-30 15:47:42 +0900

    Update networkx version to compatible version

--- plantnet_clone_1/settings.py
+++ plantnet_clone_1/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -49,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'plantnet_clone_1.urls'
@@ -76,11 +79,11 @@ WSGI_APPLICATION = 'plantnet_clone_1.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
 }
+
+# HerokuのDATABASE_URLを使用する場合
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 
 # Password validation
@@ -126,5 +129,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 # settings.py
-MODEL_PATH = os.path.join(BASE_DIR, '/Users/mame/Downloads/plantnet_300K/plant_model.h5'
+MODEL_PATH = os.path.join(BASE_DIR, '/Users/mame/plantnetvgg16_2/plantnetvgg16_2/plantnet_clone_1/data/MobileNetV2_model.keras'
 )
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
